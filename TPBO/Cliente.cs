2d12915d624f18a7280fa2dcfa6f85d132dd7f31
MiguelOliveira10260 <miguel777oliveira@hotmail.com>
2017-05-16 23:21:57 +0100

    Ultima Atualizacao

--- TPBO/Cliente.cs
+++ TPBO/Cliente.cs
@@ -7,6 +7,46 @@ namespace TPBO
     class Cliente
     {
         int numCliente;
+        string moradaCliente;
+        int numeroTelefone;
+        int numeroFiscal;
 
+        public Cliente()
+        {
+            
+        }
+        public Cliente( int numeroCliente, string endCliente, int numTel, int numFisc)
+        {
+            numCliente = numeroCliente;
+            moradaCliente = endCliente;
+            numeroTelefone = numTel;
+            numeroFiscal = numFisc;
+        }
+
+
+        public string MoradaCliente
+        {
+            get { return moradaCliente; }
+            set { moradaCliente = value; }
+        }
+        public int NumeroCliente
+        {
+            get { return numCliente; }
+            set { numCliente = value; }
+        }
+        public int NumeroTelefone
+        {
+            get { return numeroTelefone; }
+            set { numeroTelefone = value; }
+        }
+
+        public int NumeroFiscal
+        {
+            get { return numeroFiscal; }
+            set { numeroFiscal = value; }
+        }
+        
     }
+
+    
 }
